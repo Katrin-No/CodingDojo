49677fd40b78d96fc5ec4a5feab324ba517b1160
Katrin-No <69308533+Katrin-No@users.noreply.github.com>
2020-09-10 15:31:29 +0200

    Update fizzbuzz.rb
    while schleife geht auch!

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -9,3 +9,17 @@ for a in 1...100
     puts a
   end
 end
+
+a = 1
+while (a <= 100)
+  if (a % 5) == 0 && (a % 3) == 0
+    puts "FizzBuzz"
+  elsif (a % 5) == 0
+    puts "Buzz"
+  elsif (a % 3) == 0
+    puts "Fizz"
+  else
+    puts a
+  end
+  a = a + 1
+end
