2dc8dc0ae3972d83e95b6ecf1404d2f62b6196f8
Katrin-No <69308533+Katrin-No@users.noreply.github.com>
2020-09-14 12:11:47 +0200

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -15,3 +15,17 @@ let a = 1
       else console.log(a);
       a++;
     };
+
+
+// gerade und ungerade Zahlen
+for (let i = 2; i <= 10; i++) {
+  if (i % 2 == 0) {
+    alert( i );
+  }
+}
+
+for (let i = 0; i < 10; i++) {
+  if (i % 2 == 0) continue;
+  alert(i);
+  }
+}
