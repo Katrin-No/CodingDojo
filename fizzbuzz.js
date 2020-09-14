03ddfb31d288ee55af038f6379c0326e5bf98466
Katrin-No <69308533+Katrin-No@users.noreply.github.com>
2020-09-14 11:12:11 +0200

    Update and rename fizzbuzz.html to fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,7 @@
+for (var a = 1; a < 101; a++) {
+  
+  if (a % 3 == 0) console.log("Fizz");
+  else if (a % 5 == 0) console.log("Buzz");
+  else if (a % 15 == 0) console.log("FizzBuzz");
+  else console.log(a);
+} 
