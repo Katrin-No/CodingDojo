10ff0987a2d9aeeb215d682362860b27398b14fb
Katrin-No <69308533+Katrin-No@users.noreply.github.com>
2020-09-14 13:47:34 +0200

    Update fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -23,20 +23,3 @@ while (a <= 100)
   end
   a = a + 1
 end
-
-puts "gerade Zahlen"
-
-for i in 2...10
-  if (i % 2 == 0)
-    puts i
-  end
-end
-
-puts "ungerade Zahlen"
-
-for i in 0...10
-  if (i % 2 == 0)
-    next
-  end
-  puts i
-end
