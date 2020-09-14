bd4109204173d96d9eefa156b7537d5e8c675057
Katrin-No <69308533+Katrin-No@users.noreply.github.com>
2020-09-14 11:42:12 +0200

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,3 +5,13 @@ for (var a = 1; a < 101; a++) {
   else if (a % 3 == 0 && a % 5 == 0) console.log("FizzBuzz");
   else console.log(a);
 } 
+
+// mit while-Schleife
+let a = 1
+    while (a <= 100) {
+      if (a % 3 == 0) console.log("Fizz");
+      else if (a % 5 == 0) console.log("Buzz");
+      else if (a % 15 == 0) console.log("FizzBazz");
+      else console.log(a);
+      a++
+    };
