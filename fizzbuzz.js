c6c8ba9aea64dddd86d6ee2849d3fd64efffd66f
Katrin-No <69308533+Katrin-No@users.noreply.github.com>
2020-09-14 11:33:05 +0200

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,6 +2,6 @@ for (var a = 1; a < 101; a++) {
   
   if (a % 3 == 0) console.log("Fizz");
   else if (a % 5 == 0) console.log("Buzz");
-  else if (a % 15 == 0) console.log("FizzBuzz");
+  else if (a % 3 == 0 && a % 5 == 0) console.log("FizzBuzz");
   else console.log(a);
 } 
